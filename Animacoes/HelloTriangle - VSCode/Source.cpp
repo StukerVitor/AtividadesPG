23c6d92ae1f31102ca1c5eb481d379a414cf6448
stukervitor <vitoralmeid12345@outlook.com>
2024-10-12 15:55:08 -0300

    animacoes

--- Animacoes/HelloTriangle - VSCode/Source.cpp
+++ Animacoes/HelloTriangle - VSCode/Source.cpp
@@ -28,6 +28,15 @@ struct Sprite
 	vec3 position;
 	vec3 dimensions;
 	float angle;
+	// Para controle da animação
+	int nAnimations, nFrames;
+	int iAnimation, iFrame;
+	vec2 d;
+	float FPS;
+	float lastTime;
+
+	// Função de inicialização
+	void setupSprite(int texID, vec3 position, vec3 dimensions, int nFrames, int nAnimations);
 };
 
 // Protótipos das funções
@@ -58,20 +67,16 @@ const GLchar *vertexShaderSource = "#version 400\n"
 const GLchar *fragmentShaderSource = "#version 400\n"
 									 "in vec2 texCoord;\n"
 									 "uniform sampler2D texBuffer;\n"
+									 "uniform vec2 offsetTex;\n"
 									 "out vec4 color;\n"
 									 "void main()\n"
 									 "{\n"
-									 "color = texture(texBuffer, texCoord);\n"
+									 "color = texture(texBuffer, texCoord + offsetTex);\n"
 									 "}\n\0";
 
-enum directions
-{
-	NONE,
-	LEFT,
-	RIGHT
-};
-int dir = NONE;
-float vel = 2.0;
+float vel = 1.2;
+
+bool keys[1024] = {false};
 
 // Função MAIN
 int main()
@@ -80,7 +85,7 @@ int main()
 	glfwInit();
 
 	// Criação da janela GLFW
-	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Sprites - Vitor Stuker", nullptr, nullptr);
+	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Animacoes - Vitor Stuker", nullptr, nullptr);
 	if (window == nullptr)
 	{
 		cout << "Failed to create GLFW window" << endl;
@@ -118,17 +123,13 @@ int main()
 	Sprite background, character;
 	int imgWidth, imgHeight;
 
-	background.VAO = setupGeometry();
-	background.texID = loadTexture("../Texturas/backgrounds/PNG/Battleground3/Bright/Battleground3.png", imgWidth, imgHeight);
-	background.dimensions.x = imgWidth * 0.5;
-	background.dimensions.y = imgHeight * 0.5;
-	background.dimensions.z = 1.0;
-	background.position = vec3(400.0, 300.0, 0.0);
+	// Inicializando a sprite do fundo
+	int texID = loadTexture("../Texturas/backgrounds/PNG/Battleground3/Bright/Battleground3.png", imgWidth, imgHeight);
+	background.setupSprite(texID, vec3(400.0, 300.0, 0.0), vec3(imgWidth * 0.5, imgHeight * 0.5, 1.0), 1, 1);
 
-	character.VAO = setupGeometry();
-	character.texID = loadTexture("../Texturas/characters/PNG/1 Pink_Monster/Pink_Monster.png", imgWidth, imgHeight);
-	character.dimensions = vec3(imgWidth * 2.0, imgHeight * 2.0, 1.0);
-	character.position = vec3(50.0, 200.0, 0.0);
+	// Inicializando a sprite do personagem
+	texID = loadTexture("../Texturas/characters/PNG/1 Pink_Monster/Pink_Monster_Walk_6.png", imgWidth, imgHeight);
+	character.setupSprite(texID, vec3(50.0, 200.0, 0.0), vec3(imgWidth / 6.0 * 2.0, imgHeight * 2.0, 1.0), 6, 1);
 	//////////////////////////////////////////////////////////////////////////////
 
 	// Ativando o shader program
@@ -165,11 +166,25 @@ int main()
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // cor de fundo
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+		vec2 offsetTex = vec2(0.0, 0.0);
+		glUniform2f(glGetUniformLocation(shaderID, "offsetTex"), offsetTex.s, offsetTex.t);
 		drawSprite(background, shaderID);
-		if (dir == LEFT)
+		if (keys[GLFW_KEY_LEFT] || keys[GLFW_KEY_A])
 			character.position.x -= vel;
-		if (dir == RIGHT)
+		if (keys[GLFW_KEY_RIGHT] || keys[GLFW_KEY_D])
 			character.position.x += vel;
+
+		// Incremento circular (em loop) do índice do frame
+		float now = glfwGetTime();
+		float dt = now - character.lastTime;
+		if (dt >= 1.0 / character.FPS)
+		{
+			character.iFrame = (character.iFrame + 1) % character.nFrames; // incrementando ciclicamente o indice do Frame
+			character.lastTime = now;
+		}
+		offsetTex.s = character.iFrame * character.d.s;
+		offsetTex.t = 0.0;
+		glUniform2f(glGetUniformLocation(shaderID, "offsetTex"), offsetTex.s, offsetTex.t);
 		drawSprite(character, shaderID);
 
 		// Troca os buffers da tela
@@ -178,6 +193,77 @@ int main()
 
 	glfwTerminate();
 	return 0;
+}
+
+// Esta função está bastante harcoded - objetivo é criar os buffers que armazenam a
+// geometria de um triângulo
+// Apenas atributo coordenada nos vértices
+// 1 VBO com as coordenadas, VAO com apenas 1 ponteiro para atributo
+// A função retorna o identificador do VAO
+void Sprite::setupSprite(int texID, vec3 position, vec3 dimensions, int nFrames, int nAnimations)
+{
+	this->texID = texID;
+	this->dimensions = dimensions;
+	this->position = position;
+	this->nAnimations = nAnimations;
+	this->nFrames = nFrames;
+	iAnimation = 0;
+	iFrame = 0;
+
+	d.s = 1.0 / (float)nFrames;
+	d.t = 1.0 / (float)nAnimations;
+	// Aqui setamos as coordenadas x, y e z do triângulo e as armazenamos de forma
+	// sequencial, já visando mandar para o VBO (Vertex Buffer Objects)
+	// Cada atributo do vértice (coordenada, cores, coordenadas de textura, normal, etc)
+	// Pode ser arazenado em um VBO único ou em VBOs separados
+	GLfloat vertices[] = {
+		// x   y     z    s     		t
+		// T0
+		-0.5, -0.5, 0.0, 0.0, 0.0, // V0
+		-0.5, 0.5, 0.0, 0.0, d.t,  // V1
+		0.5, -0.5, 0.0, d.s, 0.0,  // V2
+		0.5, 0.5, 0.0, d.s, d.t	   // V3
+
+	};
+
+	GLuint VBO, VAO;
+	// Geração do identificador do VBO
+	glGenBuffers(1, &VBO);
+	// Faz a conexão (vincula) do buffer como um buffer de array
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	// Envia os dados do array de floats para o buffer da OpenGl
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// Geração do identificador do VAO (Vertex Array Object)
+	glGenVertexArrays(1, &VAO);
+	// Vincula (bind) o VAO primeiro, e em seguida  conecta e seta o(s) buffer(s) de vértices
+	// e os ponteiros para os atributos
+	glBindVertexArray(VAO);
+	// Para cada atributo do vertice, criamos um "AttribPointer" (ponteiro para o atributo), indicando:
+	//  Localização no shader * (a localização dos atributos devem ser correspondentes no layout especificado no vertex shader)
+	//  Numero de valores que o atributo tem (por ex, 3 coordenadas xyz)
+	//  Tipo do dado
+	//  Se está normalizado (entre zero e um)
+	//  Tamanho em bytes
+	//  Deslocamento a partir do byte zero
+
+	// Atributo 0 - Posição - x, y, z
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid *)0);
+	glEnableVertexAttribArray(0);
+
+	// Atributo 1 - Coordenadas de textura - s, t
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid *)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+
+	// Observe que isso é permitido, a chamada para glVertexAttribPointer registrou o VBO como o objeto de buffer de vértice
+	// atualmente vinculado - para que depois possamos desvincular com segurança
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// Desvincula o VAO (é uma boa prática desvincular qualquer buffer ou array para evitar bugs medonhos)
+	glBindVertexArray(0);
+	this->VAO = VAO;
+	FPS = 12.0;
+	lastTime = 0.0;
 }
 
 int loadTexture(string filePath, int &imgWidth, int &imgHeight)
@@ -193,8 +279,8 @@ int loadTexture(string filePath, int &imgWidth, int &imgHeight)
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
 	// Configurando o filtering de minificação e magnificação da textura
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
 	// Carregamento da imagem da textura
 	int nrChannels;
@@ -230,18 +316,13 @@ void key_callback(GLFWwindow *window, int key, int scancode, int action, int mod
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GL_TRUE);
 
-	if (key == GLFW_KEY_A || key == GLFW_KEY_LEFT)
+	if (action == GLFW_PRESS)
 	{
-		dir = LEFT;
+		keys[key] = true;
 	}
-	else if (key == GLFW_KEY_D || key == GLFW_KEY_RIGHT)
-	{
-		dir = RIGHT;
-	}
-
 	if (action == GLFW_RELEASE)
 	{
-		dir = NONE;
+		keys[key] = false;
 	}
 }
 
